fcbd1c3d8d3cbf84ef29f6f8255bb45230294c7d
chris_am <c.aranguizm@outlook.com>
2021-05-12 20:21:21 -0400

    trying to validate if a field is empty or not

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,31 @@
-document.getElementsByName("genre").values("male");
+console.log('Entró al html')
 
-function reset() {
-    alert("congrats");
-    document.getElementsByTagName("input").reset();
-    console.log("func+o");
+let rut = document.forms["formulario"]["rut"].value;
+let primerNombre = document.forms["formulario"]["primerNombre"].value;
+let segundoNombre = document.forms["formulario"]["primerNombre"].value;
+let primerApellido = document.forms["formulario"]["primerNombre"].value;
+let SegundoApellido = document.forms["formulario"]["primerNombre"].value;
+let fechaNacimiento = document.forms["formulario"]["primerNombre"].value;
+let genre = document.forms["formulario"]["primerNombre"].value;
+let telefono = document.forms["formulario"]["primerNombre"].value;
+let correo = document.forms["formulario"]["primerNombre"].value;
+let mensaje = document.forms["formulario"]["primerNombre"].value;
+
+function resetForm() {
+    if (rut ||
+        primerNombre ||
+        segundoNombre ||
+        primerApellido ||
+        SegundoApellido ||
+        fechaNacimiento ||
+        genre ||
+        telefono ||
+        correo ||
+        mensaje === "") {
+        alert("todos los campos deben ser llenados");
+
+    } else {
+        alert("muchas gracias, nos pondremos en contacto contigo pronto");
+
+    }
 }
